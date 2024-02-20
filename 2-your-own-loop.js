819590c73ffdef5938896e8ffcaafd14e8e5c935
Ect0spasm <gavin.stetson23@student.cart.org>
2024-02-19 23:36:38 -0800

    exercises complete

--- 2-your-own-loop.js
+++ 2-your-own-loop.js
@@ -1,3 +1,8 @@
+function loop(forl, test, update, body) {
+    for (let value = forl; test(value); update(value) ) 
+    body(value);{
+    }
+}
 // Your code here.
 
 loop(3, n => n > 0, n => n - 1, console.log);
--- 4-dominant-writing-direction.js
+++ 4-dominant-writing-direction.js
@@ -1,6 +1,14 @@
 require('./includes/scripts.js') // include the scripts used by the chapter
 
 function dominantDirection(text) {
+  let tally = countBy(text, characters => {
+    let script = characterScript(characters.codePointAt(0));
+    return script ? script.direction : "none";
+  }
+    ).filter(({name}) => name != "none");
+    
+    if (tally.length == 0) return "ltr";
+    return tally.reduce((a, b) => a.count > b.count ? a : b).name;
     // Your code here.
   }
   
